591a443c259e945f4973db2fa00c3bcf8bfd31ea
Jerin <jerinjoice18@gmail.com>
2025-07-13 21:40:33 +0530

    Added a Home button

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 function checkq1() {
     let answer=document.querySelector('input[name="question1"]:checked');
-    if(answer=21){
+    if(answer.value==="21"){
         alert("A man of culture you are;)");
     }
-    else if(answer=19){
+    else if(answer.value==="19"){
         alert("Right you are mi amigo");
     }
-    else if(answer=10011){
+    else if(answer.value==="10011"){
         alert("WRANG IT IS IN DECIMAL YOU FOOL");
     }
 
