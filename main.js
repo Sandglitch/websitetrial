65cc4b334e5d87905268c0e343ebd1384c66b1cf
Jerin <jerinjoice18@gmail.com>
2025-07-13 20:50:47 +0530

    Project:Miniquiz is over, last save before major overhaul

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
-const age=30;
-console.log(age);
-if(age>30){
-    console.log("Age is greater than 30");
-}
-else{
-    console.log("Age is less than 30");
+function checkq1() {
+    let answer=document.querySelector('input[name="question1"]:checked');
+    if(answer=21){
+        alert("A man of culture you are;)");
+    }
+    else if(answer=19){
+        alert("Right you are mi amigo");
+    }
+    else if(answer=10011){
+        alert("WRANG IT IS IN DECIMAL YOU FOOL");
+    }
+
 }
